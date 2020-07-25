17972a65d93ba28f5f6c9a4c5e2efd1e6e12c42c
Adityakumar Sinha <adityakumar113141@gmail.com>
2020-07-25 23:54:46 +0530

    Add files via upload

---
+++ Codeforces/1119E_Pavel_and_triangles.c
@@ -0,0 +1,30 @@
+    #include<stdio.h>
+     
+    int main() 
+    {
+     
+    	 long long N, count=0;
+    	long long int carry=0;
+    	scanf("%lli", &N);
+            long long int curr;
+    	while (N>0) 
+    	{	
+    		scanf("%lli", &curr);
+     
+    		while (carry>0 && curr>1)
+    	       	{
+    		        carry--;
+    			curr=curr-2;
+    			count++;
+    		}
+     
+    		count=count+(curr/3);
+    		carry=carry+(curr%3);
+    		N--;
+    	}
+     
+    	printf("%lli", count);
+     
+    	return 0;
+    }
+
